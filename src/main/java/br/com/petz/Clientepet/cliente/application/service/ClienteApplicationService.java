101b84c18befa7423266aa7d3c606bb710eb5668
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-02 13:46:53 -0300

    implementa camada de servico

--- src/main/java/br/com/petz/Clientepet/cliente/application/service/ClienteApplicationService.java
+++ src/main/java/br/com/petz/Clientepet/cliente/application/service/ClienteApplicationService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.petz.Clientepet.cliente.application.api.ClienteIdResponse;
 import br.com.petz.Clientepet.cliente.application.api.ClienteRequest;
+import br.com.petz.Clientepet.cliente.application.repository.ClienteRepository;
 import br.com.petz.Clientepet.cliente.domain.Cliente;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -13,12 +14,14 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class ClienteApplicationService implements CLienteService {
 
+	private final ClienteRepository clienteRepository;
+
 	@Override
 	public ClienteIdResponse cadastraNovoCliente(ClienteRequest clienteRequest) {
 		log.info("[inicia] ClienteApplicationService - cadastraNovoCliente");
 		Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
 		log.info("[finaliza] ClienteApplicationService - cadastraNovoCliente");
-		return cliente;
+		return ClienteIdResponse.builder().idCliente(cliente.getIdCliente()).build();
 	}
 
 }
