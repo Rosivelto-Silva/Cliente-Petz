d16a43a12816794ab63a3223d294fd4f88669efe
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-02 13:27:03 -0300

    implementa ClienteRequest

--- src/main/java/br/com/petz/Clientepet/cliente/application/api/ClienteRequest.java
+++ src/main/java/br/com/petz/Clientepet/cliente/application/api/ClienteRequest.java
@@ -1,5 +1,20 @@
 package br.com.petz.Clientepet.cliente.application.api;
 
-public class ClienteRequest {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import br.com.petz.Clientepet.cliente.domain.Sexo;
+import lombok.Getter;
+
+@Getter
+public class ClienteRequest {
+	private String nomeCompleto;
+	private String email;
+	private String celular;
+	private String telefone;
+	private String cpf;
+	private Sexo sexo;
+	private LocalDate dataNascimento;
+	private LocalDateTime dataHoraDoCadastro;
+	private LocalDateTime dataHoraDaUltimaAtualizacao;
 }
