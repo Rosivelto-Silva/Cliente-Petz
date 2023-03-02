a35cb61b89ffb311368fff5d9d51b91ac379f985
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-02 14:18:14 -0300

    implementa camada de Repository

---
+++ src/main/java/br/com/petz/Clientepet/cliente/application/repository/ClienteInfraRepository.java
@@ -0,0 +1,24 @@
+package br.com.petz.Clientepet.cliente.application.repository;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.petz.Clientepet.cliente.domain.Cliente;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Repository
+@Log4j2
+@RequiredArgsConstructor
+public class ClienteInfraRepository implements ClienteRepository {
+
+	private final SpringDataJpaRepository repository;
+
+	@Override
+	public Cliente salva(Cliente cliente) {
+		log.info("[inicia] ClienteInfraRepository - salva");
+		Cliente salvaCliente = repository.save(cliente);
+		log.info("[finaliza] ClienteInfraRepository - salva");
+		return salvaCliente;
+	}
+
+}
