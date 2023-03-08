dcde32f5281c1ccdc70793ea0948bd882889c9b9
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2023-03-08 02:08:07 -0300

    implementa camada de API

--- src/main/java/br/com/petz/Clientepet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/Clientepet/cliente/application/api/ClienteAPI.java
@@ -1,9 +1,11 @@
 package br.com.petz.Clientepet.cliente.application.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,10 @@ public interface ClienteAPI {
 	ClienteIdResponse postCadastraNovoCliente(@RequestBody ClienteRequest clienteRequest); 
 	
 	@GetMapping
-	@ResponseStatus
+	@ResponseStatus(value = HttpStatus.OK)
 	List<ClienteListResponse> getListaTodosClientes();
+	
+	@GetMapping("{idCliente}")
+	@ResponseStatus(value = HttpStatus.OK)
+	ClienteDetalhadoResponse getVisualizaDetalhesCliente(@PathVariable UUID idCliente);
 }
